7c0d1fd1a42034576d0558e11db65f832819fc0b
rtorkiharchega <r.torkiharchegani@tudelft.nl>
2025-06-12 15:39:49 +0200

    manifest and icon

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = "survey-app-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./images/app-icon.png",
+  "./manifest.json",
+  "https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap",
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
